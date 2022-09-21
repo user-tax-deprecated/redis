4fc67a3e9de4eb52992f1c061881ecbf78364c21
usrtax <i@user.tax>
2022-09-21 08:37:27 +0800

    ♨

--- index.d.ts
+++ index.d.ts
@@ -17,7 +17,8 @@ export class Redis {
   mdel(keyLi: Array<Bin>): Promise<number>
   exist(key: Bin): Promise<boolean>
   mexist(key: Array<Bin>): Promise<number>
-  hget(key: Bin, field: Bin): Promise<Uint8Array | null>
+  hget(key: Bin, field: Bin): Promise<string | null>
+  hgetB(key: Bin, field: Bin): Promise<Uint8Array | null>
   hset(key: Bin, map: Record<string, Bin>): Promise<void>
   hincrby(key: Bin, field: Bin, increment: number): Promise<number>
   hincr(key: Bin, field: Bin): Promise<number>
