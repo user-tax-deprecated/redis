9c1dd8d507108459132f4c5a9995ff5fbee02c62
usrtax <i@user.tax>
2022-09-22 17:05:34 +0800

    ♨

--- src/bin.rs
+++ src/bin.rs
@@ -5,7 +5,7 @@ use fred::{
   types::{MultipleValues, RedisKey, RedisValue},
 };
 use napi::{
-  bindgen_prelude::{Buffer, FromNapiValue},
+  bindgen_prelude::{Buffer, FromNapiValue, TypeName, ValidateNapiValue, ValueType},
   sys::{napi_env, napi_value},
   Either,
 };
@@ -17,6 +17,21 @@ pub struct Bin(StringOrBuffer);
 impl AsRef<[u8]> for Bin {
   fn as_ref(&self) -> &[u8] {
     self.0.as_ref()
+  }
+}
+
+impl TypeName for Bin {
+  fn type_name() -> &'static str {
+    "String/Buffer/Uint8Array"
+  }
+  fn value_type() -> napi::ValueType {
+    StringOrBuffer::value_type()
+  }
+}
+
+impl ValidateNapiValue for Bin {
+  unsafe fn validate(env: napi_env, napi_val: napi_value) -> napi::Result<napi_value> {
+    unsafe { StringOrBuffer::validate(env, napi_val) }
   }
 }
 
@@ -44,28 +59,4 @@ impl From<Bin> for RedisValue {
   }
 }
 
-pub type EitherBinVec = Either<Bin, Vec<Bin>>;
-pub struct BinMaybeVec(EitherBinVec);
-
-impl FromNapiValue for BinMaybeVec {
-  unsafe fn from_napi_value(env: napi_env, napi_val: napi_value) -> napi::Result<Self> {
-    Ok(BinMaybeVec(EitherBinVec::from_napi_value(env, napi_val)?))
-  }
-}
-
-impl IntoIterator for BinMaybeVec {
-  type Item = Bin;
-  type IntoIter = std::vec::IntoIter<Self::Item>;
-  fn into_iter(self) -> Self::IntoIter {
-    self.0.into_iter()
-  }
-}
-
-impl From<BinMaybeVec> for RedisKey {
-  fn from(t: BinMaybeVec) -> RedisKey {
-    match t.0 {
-      EitherBinVec::A(a) => a.into(),
-      EitherBinVec::B(b) => b.into(),
-    }
-  }
-}
+pub type BinMaybeVec = Either<Bin, Vec<Bin>>;
